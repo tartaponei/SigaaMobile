a5c7ab1a16557cc640f5872406d862c2360a3606
sarah <sarahborretti@gmail.com>
2025-02-27 21:00:36 -0300

    Começo da construção da página de login

--- src/app/auth/shared/header/header.component.ts
+++ src/app/auth/shared/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { InlineSVGModule } from 'ng-inline-svg-2';
 import { HttpClientModule } from '@angular/common/http';
+import { DarkModeSwitchComponent } from '../../../shared/dark-mode-switch/dark-mode-switch.component';
 
 @Component({
   selector: 'app-header',
-  imports: [InlineSVGModule, HttpClientModule],
+  imports: [InlineSVGModule, HttpClientModule, DarkModeSwitchComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
---
+++ src/app/shared/dark-mode-switch/dark-mode-switch.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dark-mode-switch',
+  imports: [],
+  templateUrl: './dark-mode-switch.component.html',
+  styleUrl: './dark-mode-switch.component.css'
+})
+export class DarkModeSwitchComponent {
+
+}
